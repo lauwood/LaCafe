b59397e4c1add6ee01a5a591abf7ddbd2652d079
Lauwood <varyox@gmail.com>
2015-06-29 15:57:36 -0700

    Added one second delay for clicking.

--- LauCafe/PlayState.cpp
+++ LauCafe/PlayState.cpp
@@ -111,7 +111,7 @@ void PlayState::Input() {
 		m_Camera->SetYaw(m_Camera->GetYaw() + m_Camera->GetSpeed() * -1 *  TimeManager::Instance().DeltaTime);
 	}
 	if (glfwGetKey(window, GLFW_KEY_RIGHT)) {
-		m_Camera->SetYaw(m_Camera->GetYaw() + m_Camera->GetSpeed() *  TimeManager::Instance().DeltaTime);
+		m_Camera->SetYaw(m_Camera->GetYaw() + m_Camera->GetSpeed() * TimeManager::Instance().DeltaTime);
 	}
 
 	// Mouse motions
@@ -122,50 +122,54 @@ void PlayState::Input() {
 	double dx = nx - MouseX;
 	double dy = ny - MouseY;*/
 
+	static double clickStartTime = TimeManager::Instance().GetTime();
+
 	// Raycast
 	if (MouseActiveButton & MOUSE_LEFT) {
-		double nx, ny;
+		if (TimeManager::Instance().CurrentTime - clickStartTime > 1.0f) {
+			clickStartTime = TimeManager::Instance().CurrentTime;
+			double nx, ny;
 
-		glfwGetCursorPos(window, &nx, &ny);
+			glfwGetCursorPos(window, &nx, &ny);
 
-		vec3 ray_wor = GetRayFromMouse((float)nx, (float)ny, WinX, WinY, m_Camera);
+			vec3 ray_wor = GetRayFromMouse((float)nx, (float)ny, WinX, WinY, m_Camera);
 
-		int closest_square_clicked = -1;
-		float closest_intersection = 0.0f;
-		for (int i = 0; i < NUM_OF_SQUARES; i++) {
-			float t_dist = 0.0f;
-			if (RayIntersect(m_Camera->GetPosition(), ray_wor, g_SquarePath->at(i).GetPosition(), SquareRadius, &t_dist)) {
-				// if more than one sphere is in path of ray, only use the closest one
-				if (-1 == closest_square_clicked || t_dist < closest_intersection) {
-					closest_square_clicked = i;
-					closest_intersection = t_dist;
+			int closest_square_clicked = -1;
+			float closest_intersection = 0.0f;
+			for (int i = 0; i < NUM_OF_SQUARES; i++) {
+				float t_dist = 0.0f;
+				if (RayIntersect(m_Camera->GetPosition(), ray_wor, g_SquarePath->at(i).GetPosition(), SquareRadius, &t_dist)) {
+					// if more than one sphere is in path of ray, only use the closest one
+					if (-1 == closest_square_clicked || t_dist < closest_intersection) {
+						closest_square_clicked = i;
+						closest_intersection = t_dist;
 
-					// Clear the previous paths
-					for (int k = 0; k < a->getHeight(); k++)
-						for (int l = 0; l < a->getWidth(); l++) {
+						// Clear the previous paths
+						for (int k = 0; k < a->getHeight(); k++)
+							for (int l = 0; l < a->getWidth(); l++) {
 							if (a->getTileType(k, l) == TABLE)
 								a->setTile(k, l, 0);
 							g_SquarePath->at(l + a->getHeight() * k).Unpath();
-						}
-
-					// To only allow one destination
-					if (a->getTileType(closest_square_clicked / 10, closest_square_clicked % 10) == WALKABLE)
-						a->setTile(closest_square_clicked / 10, closest_square_clicked % 10, TABLE);
-
-					a->fillPaths();
-
-					Cell* c = nullptr;
-					for (int z = 0; z < a->getHeight(); z++)
-						for (int x = 0; x < a->getWidth(); x++)
-							if (a->getTileType(z, x) == START) {
-								c = new Cell;
-								c->x = x;
-								c->z = z;
 							}
 
-					if (c != nullptr) {
+						// To only allow one destination
+						if (a->getTileType(closest_square_clicked / 10, closest_square_clicked % 10) == WALKABLE)
+							a->setTile(closest_square_clicked / 10, closest_square_clicked % 10, TABLE);
+
+						a->fillPaths();
+
+						Cell* c = nullptr;
 						for (int z = 0; z < a->getHeight(); z++)
-							for (int x = 0; x < a->getWidth(); x++) {
+							for (int x = 0; x < a->getWidth(); x++)
+								if (a->getTileType(z, x) == START) {
+							c = new Cell;
+							c->x = x;
+							c->z = z;
+								}
+
+						if (c != nullptr) {
+							for (int z = 0; z < a->getHeight(); z++)
+								for (int x = 0; x < a->getWidth(); x++) {
 								if (a->getTileType(z, x) == TABLE) {
 									// If the cell is a potential destination, print the path
 									deque<Cell*> p = a->getCellPath(c->z, c->x, z, x);
@@ -176,15 +180,16 @@ void PlayState::Input() {
 										g_SquarePath->at(top->x + a->getHeight() * top->z).Path();
 									}
 								}
-							}
-						delete c;
+								}
+							delete c;
+						}
 					}
 				}
-			}
-		} // endfor
-		SelectedSquare = closest_square_clicked;
-		g_Patron.findNextDestination();
-		//fprintf(stdout, "center of square: x: %.2f, z: %.2f", g_SquarePath->at(SelectedSquare).GetPosition().x, g_SquarePath->at(SelectedSquare).GetPosition().z);
+			} // endfor
+			SelectedSquare = closest_square_clicked;
+			g_Patron.findNextDestination();
+			//fprintf(stdout, "center of square: x: %.2f, z: %.2f", g_SquarePath->at(SelectedSquare).GetPosition().x, g_SquarePath->at(SelectedSquare).GetPosition().z);
+		}
 	}
 
 	glfwPollEvents();
